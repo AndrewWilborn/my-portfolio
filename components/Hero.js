5768a5ab36f7d39729f9fb9552bdf548bbfb6953
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-12 16:22:25 -0400

    UPDATED hero

--- components/Hero.js
+++ components/Hero.js
@@ -7,10 +7,8 @@ export default function Hero() {
           <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
-            <br className="hidden lg:inline-block" />readymade gluten
-          </h1>
-          <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Andrew Wilborn</h1>
+          <p className="mb-8 leading-relaxed">I am a Software Engineer.</p>
           <div className="flex justify-center">
             <button className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
             <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
